25cfea561c88b23b869563ff60300b0f6a8c816c
Andrey <65175184+DoroshukA@users.noreply.github.com>
2020-08-08 14:30:30 +0300

    08_08

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mmongoose = require('mongoose');
+const passsport = require('passport');
+const path = require('path');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res)=>{
+    res.send('Home page');
+});
+
+app.listen(port, ()=> {
+    console.log(`Сервер был запущен по порту: ${port}`);
+});

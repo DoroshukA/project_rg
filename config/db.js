adf8591e9d26b3002c9220037a69d56ba2568d6f
Andrey <65175184+DoroshukA@users.noreply.github.com>
2020-08-08 14:43:05 +0300

    add config

---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    db: 'mongodb://localhost:27017/rg_db',
+    secret: 'some_key'
+}
--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser');
 const mmongoose = require('mongoose');
 const passsport = require('passport');
 const path = require('path');
+const config = require('./config/db');
 
 const app = express();
 
 const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res)=>{
     res.send('Home page');
